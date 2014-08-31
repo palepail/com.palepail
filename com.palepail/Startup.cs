7620b77b72b7c284e09b20241c3f598e5d449b57
palepail <palepail@gmail.com>
2014-08-31 10:21:54 -1000

    New Project

---
+++ com.palepail/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(com.palepail.Startup))]
+namespace com.palepail
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
